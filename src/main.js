4132bc648af407b6f955f88e53e721796790c797
克劳福德重新焊 <2324147709@qq.com>
2022-03-25 10:25:38 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import ElementUI from "element-ui";
+import "./styles.scss";
 
-Vue.config.productionTip = false
+// 引入全局样式表
+import "./assets/CSS/index.css";
+
+// 引入字体图标库
+import "./assets/fonts/iconfont.css";
+
+// 引入axios
+import axios from "axios";
+// 配置请求的根路径
+axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/";
+Vue.prototype.$http = axios;
+
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false;
 
 new Vue({
-    router,
-    render: h => h(App)
-}).$mount('#app')
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
